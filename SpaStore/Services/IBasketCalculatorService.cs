25f1915210f85fcc48c998692cd2279db06377f7
Mark Blandford <mark@markblandford.co.uk>
2018-04-21 08:00:14 +0100

    Test stubs added for the new Calculator service which will calculate the basket total value.
    * Introduced the concept of an offer.

---
+++ SpaStore/Services/IBasketCalculatorService.cs
@@ -0,0 +1,14 @@
+﻿namespace SpaStore.Services
+{
+    using System.Collections.Generic;
+    using Models;
+
+    /// <summary> Service to calculate a basket. </summary>
+    public interface IBasketCalculatorService
+    {
+        /// <summary> Calculate the total value of a basket. </summary>
+        /// <param name="basket"> The basket to calculate the total for. </param>
+        /// <returns> The total price. </returns>
+        decimal CalculateBasketPrice(IBasket basket);
+    }
+}
