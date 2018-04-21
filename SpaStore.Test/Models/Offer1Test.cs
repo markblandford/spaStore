43411d3dc028fd18db8d47166aae10d1abbcda0f
Mark Blandford <mark@markblandford.co.uk>
2018-04-21 08:52:20 +0100

    Enhanced IOffer to contain some basic rules. This should eventually be combined with the price into an lambda expression.
    * Added a new Offer1 class, which is the first offer.
    * Again, this should be redundant once we move to lambda expressions (if possible) but this is to just get it working first.

---
+++ SpaStore.Test/Models/Offer1Test.cs
@@ -0,0 +1,19 @@
+﻿namespace SpaStore.Test.Models
+{
+    using SpaStore.Models;
+    using Xunit;
+
+    public class Offer1Test
+    {
+        [Fact]
+        public void Constructor_SetsTheDefaultValues()
+        {
+            IOffer cut = new Offer1();
+
+            Assert.Equal("Buy 2 Butter and get a Bread at 50% off", cut.OfferDescription);
+            Assert.Equal(2.1m, cut.OfferPrice);
+            Assert.NotNull(cut.OfferRules);
+            Assert.NotNull(cut.ProductsInOffer);
+        }
+    }
+}
---
+++ SpaStore/Models/Offer1.cs
@@ -0,0 +1,31 @@
+﻿namespace SpaStore.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    /// <summary> Offer 1. </summary>
+    public class Offer1 : IOffer
+    {
+        /// <inheritdoc />
+        public int Id { get; }
+
+        /// <inheritdoc />
+        public IList<Tuple<int, int>> OfferRules { get; }
+
+        /// <inheritdoc />
+        public IList<IProduct> ProductsInOffer { get; set; }
+
+        /// <inheritdoc />
+        public string OfferDescription { get; }
+
+        /// <inheritdoc />
+        public decimal OfferPrice { get; }
+
+        /// <inheritdoc />
+        public bool OfferSatisfied()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
