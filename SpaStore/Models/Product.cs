9b42114e069acb91c643d7e70a670852aee0bfcc
Mark Blandford <mark@markblandford.co.uk>
2018-04-21 07:06:07 +0100

    Initial structure set.
    * StyleCop ruleset applied (copied from another project).
    * Basket & Product scaffolding added.

---
+++ SpaStore/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace SpaStore.Models
+{
+    public class Product : IProduct
+    {
+        /// <inheritdoc />
+        public int Id { get; set; }
+
+        /// <inheritdoc />
+        public string Name { get; set; }
+
+        /// <inheritdoc />
+        public decimal Price { get; set; }
+    }
+}
