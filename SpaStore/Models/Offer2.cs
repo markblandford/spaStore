bc2e974634ab299e47501375139e4d6960e38426
Mark Blandford <mark@markblandford.co.uk>
2018-04-21 12:51:30 +0100

    Both offers have been applied and tests are now green.
    * I'm not entirely happy with the implementation, definitely some refactoring is necessary but let's just get something working whilst I have the time.

--- SpaStore/Models/Offer2.cs
+++ SpaStore/Models/Offer2.cs
@@ -4,20 +4,19 @@
     using System.Collections.Generic;
     using System.Linq;
 
-    /// <summary> Offer 1. </summary>
-    public class Offer1 : IOffer
+    /// <summary> Offer 2. </summary>
+    public class Offer2 : IOffer
     {
         /// <summary> Constructor. </summary>
-        public Offer1()
+        public Offer2()
         {
-            Id = 1;
-            OfferDescription = "Buy 2 Butter and get a Bread at 50% off";
-            OfferPrice = 2.1m;
+            Id = 2;
+            OfferDescription = "Buy 3 Milk and get the 4th milk for free";
+            OfferPrice = 3.45m;
 
             OfferRules = new List<Tuple<int, int>>
             {
-                new Tuple<int, int>(1, 2),
-                new Tuple<int, int>(3, 1)
+                new Tuple<int, int>(2, 4)
             };
 
             ProductsInOffer = new List<IProduct>();
@@ -41,8 +40,7 @@
         /// <inheritdoc />
         public void CheckOfferConditions()
         {
-            bool filled = ProductsInOffer.Count(p => p.Id == OfferRules[0].Item1) == OfferRules[0].Item2
-                   && ProductsInOffer.Count(p => p.Id == OfferRules[1].Item1) == OfferRules[1].Item2;
+            bool filled = ProductsInOffer.Count(p => p.Id == OfferRules[0].Item1) == OfferRules[0].Item2;
 
             OfferSatisfied = filled;
         }
