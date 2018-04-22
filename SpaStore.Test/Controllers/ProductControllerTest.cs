7eeebb522b02c61f293c0749c5872c8023969d6f
Mark Blandford <mark@markblandford.co.uk>
2018-04-22 17:55:16 +0100

    Added a product controller to get the list of products.

---
+++ SpaStore.Test/Controllers/ProductControllerTest.cs
@@ -0,0 +1,42 @@
+﻿namespace SpaStore.Test.Controllers
+{
+    using System.Collections.Generic;
+    using NSubstitute;
+    using SpaStore.Controllers;
+    using SpaStore.Models;
+    using SpaStore.Services;
+    using Xunit;
+
+    public class ProductControllerTest
+    {
+        private readonly IProductService fakeProductService;
+        private readonly ProductController controller;
+
+        public ProductControllerTest()
+        {
+            fakeProductService = Substitute.For<IProductService>();
+            fakeProductService.GetProducts().ReturnsForAnyArgs(new List<Product>
+            {
+                new Product
+                {
+                    Id = 1,
+                    Name = "Butter",
+                    Price = 0.8m
+                }
+            });
+
+            controller = new ProductController(fakeProductService);
+        }
+
+        [Fact]
+        public void GetProducts_ReturnsTheProducts()
+        {
+            var result = controller.GetProducts();
+
+            Assert.Equal(1, result[0].Id);
+            Assert.Equal("Butter", result[0].Name);
+            Assert.Equal(0.8m, result[0].Price);
+        }
+
+    }
+}
---
+++ SpaStore/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿
+namespace SpaStore.Controllers
+{
+    using System.Collections.Generic;
+    using Microsoft.AspNetCore.Mvc;
+    using Models;
+    using Services;
+
+    public class ProductController : Controller
+    {
+        private readonly IProductService productService;
+
+        /// <summary> Constructor. </summary>
+        /// <param name="productService"> injected service. </param>
+        public ProductController(IProductService productService)
+        {
+            this.productService = productService;
+        }
+
+        /// <summary> Get the products. </summary>
+        /// <returns> The products. </returns>
+        [HttpGet("product")]
+        public IList<Product> GetProducts()
+        {
+            return productService.GetProducts();
+        }
+    }
+}
