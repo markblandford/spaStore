9197d3571a17fcdc0e5b228c400b89574bc2a621
Mark Blandford <mark@markblandford.co.uk>
2018-04-22 21:09:12 +0100

    * Added some comments on the home page. * Added the required route for the new Product Controller.

--- SpaStore/ClientApp/app/components/home/home.component.ts
+++ SpaStore/ClientApp/app/components/home/home.component.ts
@@ -5,4 +5,57 @@ import { Component } from '@angular/core';
     templateUrl: './home.component.html'
 })
 export class HomeComponent {
+
+    public getProductsResponse = `
+    [
+	    {
+		    "id": 1,
+		    "name": "Butter",
+		    "price": 0.8
+	    },
+	    {
+		    "id": 2,
+		    "name": "Milk",
+		    "price": 1.15
+	    },
+	    {
+		    "id": 3,
+		    "name": "Bread",
+		    "price": 1.0
+	    }
+    ]`;
+
+    public putBasketBody = `
+    {
+        "id": null,
+        "items": [
+            {
+                "id": 3,
+                "name": "bread",
+                "price": 1
+            },
+            {
+                "id": 1,
+                "name": "butter",
+                "price": 0.8
+            }
+        ]
+    }`;
+
+    public putBasketResponse = `
+    {
+        "id": "fa25be2f-0b59-4e8d-8217-af7c1da42b16",
+        "items": [
+            {
+                "id": 3,
+                "name": "Bread",
+                "price": 1.0
+            },
+            {
+                "id": 1,
+                "name": "Butter",
+                "price": 0.8
+            }
+        ]
+    }`;
 }
